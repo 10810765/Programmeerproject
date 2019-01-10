54a8e163b487f3d1e9e1940782f215d36d55918f
Marijn Meijering <rijno95@hotmail.com>
2019-01-10 16:01:24 +0100

    Some buttons and functionalities added

--- app/src/main/java/com/example/marijn/esportsapp/CategoriesActivity.java
+++ app/src/main/java/com/example/marijn/esportsapp/CategoriesActivity.java
@@ -1,7 +1,10 @@
 package com.example.marijn.esportsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CategoriesActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class CategoriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+    }
+
+    // On button click, go to Overview Activity
+    public void onCategoryClicked(View view){
+        Button pressedGame = findViewById(view.getId());
+        String categoryId = String.valueOf(pressedGame.getTag());
+
+        // Pass the chosen category and the users name to the next activity
+        Intent intent = new Intent(this, OverviewActivity.class);
+        intent.putExtra("category_id", categoryId);
+        startActivity(intent);
     }
 }
