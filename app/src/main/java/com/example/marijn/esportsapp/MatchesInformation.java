6f285373c878d25453723cd8077a1b99fda606e3
10810765 <10810765@users.noreply.github.com>
2019-01-11 14:26:14 +0100

    Matches constructor class created

---
+++ app/src/main/java/com/example/marijn/esportsapp/MatchesInformation.java
@@ -0,0 +1,48 @@
+package com.example.marijn.esportsapp;
+
+public class MatchesInformation {
+
+    private String date, title, eventUrl, imageUrl;
+
+    // Matches information constructor
+    public MatchesInformation(String date, String title, String eventUrl, String imageUrl) {
+        this.date = date;
+        this.title = title;
+        this.eventUrl = eventUrl;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getters and setters
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(String eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
+
--- app/src/main/java/com/example/marijn/esportsapp/MatchesRequest.java
+++ app/src/main/java/com/example/marijn/esportsapp/MatchesRequest.java
@@ -1,4 +1,93 @@
 package com.example.marijn.esportsapp;
 
-public class MatchesRequest {
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MatchesRequest implements Response.Listener<JSONObject>, Response.ErrorListener {
+    private Context context;
+    private Callback activity;
+
+    // Notify the activity that instantiated the request through callback
+    public interface Callback {
+        void gotTrivia(ArrayList<String> trivia);
+        void gotTriviaError(String message);
+    }
+
+    // Create context object to send internet requests
+    public MatchesRequest(Context context) {
+        this.context = context;
+    }
+
+    // Method used to retrieve questions from API
+    public void getTrivia(Callback activity, String category) {
+        this.activity = activity;
+
+        // Create a new request queue
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        // Create a JSON object request and add it to the queue
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("https://opentdb.com/api.php?amount=10&category="+category+"&type=multiple", null, this, this);
+        queue.add(jsonObjectRequest);
+    }
+
+    @Override // Handle on API error response
+    public void onErrorResponse(VolleyError error) {
+        activity.gotTriviaError(error.getMessage());
+        Log.d("goTriviaError", error.getMessage());
+    }
+
+    @Override // Handle on API response
+    public void onResponse(JSONObject response) {
+        try {
+            // Instantiate array list
+            ArrayList<Trivia> triviaArrayList = new ArrayList<>();
+
+            JSONArray resultsArray = response.getJSONArray("results");
+
+            // Loop over the JSON array and extract the strings in it
+            for (int i = 0; i < resultsArray.length(); i++) {
+
+                JSONObject resultsObject = resultsArray.getJSONObject(i);
+
+                String difficulty = resultsObject.getString("difficulty");
+                String question = resultsObject.getString("question");
+                String correctAnswer = resultsObject.getString("correct_answer");
+                String ID = String.valueOf(i);
+
+                // Instantiate a second array list (used for the wrong answers per question)
+                ArrayList<String> incAnsArrayList = new ArrayList<>();
+
+                JSONArray incAnsArray = resultsObject.getJSONArray("incorrect_answers");
+
+                // Loop over the incorrect answer JSON array and extract the strings
+                for (int j = 0; j < incAnsArray.length(); j++) {
+                    // Add the incorrect answers to the incAnsArrayList
+                    incAnsArrayList.add(incAnsArray.getString(j));
+                }
+
+                // Add the information to the Trivia array list
+                triviaArrayList.add(new Trivia(difficulty, question, correctAnswer, incAnsArrayList, ID));
+
+            }
+            // Pass the array list back to the activity that requested it
+            activity.gotTrivia(triviaArrayList);
+
+        } catch (JSONException e) {
+            // If an error occurs, print the error
+            e.printStackTrace();
+        }
+    }
 }
+
