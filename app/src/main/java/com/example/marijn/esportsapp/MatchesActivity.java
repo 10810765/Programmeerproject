f5d77bd8103ee71e8715df9c03d0c0f8b8fcd865
10810765 <10810765@users.noreply.github.com>
2019-01-14 12:08:02 +0100

    MatchesAdapter almost working

--- app/src/main/java/com/example/marijn/esportsapp/MatchesActivity.java
+++ app/src/main/java/com/example/marijn/esportsapp/MatchesActivity.java
@@ -2,12 +2,24 @@ package com.example.marijn.esportsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MatchesActivity extends AppCompatActivity {
+
+    private ArrayList<MatchesInformation> matches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches);
+
+        // Instantiate the adapter
+        MatchesAdapter matchAdapter = new MatchesAdapter(this, R.layout.match_row, matches);
+
+        // Get list view ID and attach the adapter to it
+        ListView matchList = findViewById(R.id.matchList);
+        matchList.setAdapter(matchAdapter);
     }
 }
--- app/src/main/java/com/example/marijn/esportsapp/MatchesAdapter.java
+++ app/src/main/java/com/example/marijn/esportsapp/MatchesAdapter.java
@@ -1,4 +1,49 @@
 package com.example.marijn.esportsapp;
 
-public class MatchesAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MatchesAdapter extends ArrayAdapter<MatchesInformation> {
+
+    private ArrayList matches;
+
+    public MatchesAdapter(@NonNull Context context, int resource, @NonNull ArrayList<MatchesInformation> objects) {
+        super(context, resource, objects);
+        this.matches = objects;
+    }
+
+    @NonNull
+    @Override // Method that will be called every time a new list item (menu item) is to be displayed
+    public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent) {
+
+        // If the convert view is null, inflate a new one
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.match_row, parent, false);
+        }
+
+        // Get the ID's of the name and price (TextView) and the picture (ImageView)
+        TextView date = convertView.findViewById(R.id.dateView);
+        TextView title = convertView.findViewById(R.id.titleView);
+        TextView teams = convertView.findViewById(R.id.teamsView);
+        TextView link = convertView.findViewById(R.id.urlView);
+
+        // Get the index of the menu item that we want to display
+        MatchesInformation matchInfo = (MatchesInformation) matches.get(position);
+
+        // Set the name and price of the dish
+        date.setText(matchInfo.getDate());
+        title.setText(matchInfo.getTitle());
+        teams.setText(matchInfo.getTeams());
+        link.setText(matchInfo.getEventUrl());
+
+        return convertView;
+    }
 }
