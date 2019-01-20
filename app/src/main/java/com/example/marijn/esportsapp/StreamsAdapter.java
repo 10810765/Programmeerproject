911cef85163b822a61c20b267f75cfb1524a8741
Marijn Meijering <rijno95@hotmail.com>
2019-01-20 20:30:23 +0100

    First attempt at creating a favourite button inside the streams list view

--- app/src/main/java/com/example/marijn/esportsapp/StreamsAdapter.java
+++ app/src/main/java/com/example/marijn/esportsapp/StreamsAdapter.java
@@ -2,18 +2,24 @@ package com.example.marijn.esportsapp;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class StreamsAdapter extends ArrayAdapter<StreamsInformation> {
+
+    private ToggleButton favouriteButton;
 
     public StreamsAdapter(@NonNull Context context, int resource, @NonNull ArrayList<StreamsInformation> objects) {
         super(context, resource, objects);
@@ -36,6 +42,7 @@ public class StreamsAdapter extends ArrayAdapter<StreamsInformation> {
         TextView name = convertView.findViewById(R.id.nameView);
         TextView views = convertView.findViewById(R.id.viewersView);
         ImageView logo = convertView.findViewById(R.id.logoView);
+        favouriteButton = convertView.findViewById(R.id.favouriteButton);
 
         // Set the name, title and viewer count of the streamer
         title.setText(streamInfo.getTitle());
@@ -44,6 +51,16 @@ public class StreamsAdapter extends ArrayAdapter<StreamsInformation> {
 
         // Load image from the internet into an image view using Picasso
         Picasso.get().load(streamInfo.getImageUrl()).into(logo);
+
+        favouriteButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.grey_star));
+
+//        favouriteButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked)
+//                    favouriteButton.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.yellow_star));
+//
+//            }
+//    });
 
         return convertView;
     }
